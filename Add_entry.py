a6f3eee03b8752902ae6ae76a856dcbafb6a1338
pochita0109 <akosicosta@gmail.com>
2023-07-22 15:11:57 -0700

    Created new file for add entry, added pseudocodes

---
+++ Add_entry.py
@@ -0,0 +1,10 @@
+# Ask the name
+# Ask the date of birth
+# Ask the address
+# Ask the phone number
+# Ask the email adress
+# Ask the date of visit
+# Ask the vaccination status
+# Ask if there are symptoms of COVID-19
+# Ask if there are exposure on someone with COVID-19
+# Ask if tested positive on COVID-19 in the last 14 days
