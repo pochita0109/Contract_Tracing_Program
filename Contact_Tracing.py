e3037184f6068100aaf79a38b0e80556dca56640
pochita0109 <akosicosta@gmail.com>
2023-07-22 16:38:04 -0700

    Changed the main page into class

--- Contact_Tracing.py
+++ Contact_Tracing.py
@@ -1,15 +1,17 @@
 import tkinter as tk
-from tkinter import *
 
-front = tk.Tk()
-front.title("Contact Tracing")
-front.geometry("1000x550")
+class main_page(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Contact Tracing")
+        self.geometry("1000x550")
+        
+        self.add = tk.Button(self, text = "   Add Entry   ")
+        self.add.place(x = 465, y = 225)
 
-add = Button(front, text = "    Add Entry   ") 
-add.place(x=465,y=225)
-
-search = Button(front, text = "  Search Entry  ")
-search.place(x=465,y=300)
-
-front.mainloop()
-
+        self.search = tk.Button(self, text = "   Search Entry   ")
+        self.search.place(x = 465, y = 300)
+        
+if __name__=="__main__":
+    app = main_page()
+    app.mainloop()
