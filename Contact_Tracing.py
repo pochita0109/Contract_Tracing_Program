5ccf455928cc0c06b67d50e0619a4cdef926d030
pochita0109 <akosicosta@gmail.com>
2023-07-22 14:55:16 -0700

    Added add entry and search entry button

--- Contact_Tracing.py
+++ Contact_Tracing.py
@@ -1,8 +1,15 @@
 import tkinter as tk
+from tkinter import *
 
 front = tk.Tk()
 front.title("Contact Tracing")
-front.geometry("600x600")
+front.geometry("1000x550")
+
+add = Button(front, text = "    Add Entry   ") 
+add.place(x=465,y=225)
+
+search = Button(front, text = "  Search Entry  ")
+search.place(x=465,y=300)
 
 front.mainloop()
 
