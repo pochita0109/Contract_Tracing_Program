d145ce29a56277135c8637a6ce4ece2d26add982
pochita0109 <akosicosta@gmail.com>
2023-07-24 15:42:43 -0700

    Added label and entry for email address, and changed the position of labels and entries of other questions

--- Add_entry.py
+++ Add_entry.py
@@ -6,33 +6,39 @@ class add_entry(tk.Frame):
 
         # Ask the name
         self.name = tk.Label(self, text = "Name")
-        self.name.place(x = 50, y = 50)
+        self.name.place(x = 65, y = 50)
 
         self.name_entry = tk.Entry(self)
-        self.name_entry.place(x = 90, y = 50)
+        self.name_entry.place(x = 105, y = 50)
 
         # Ask the date of birth
         self.birth = tk.Label(self, text = "Date of Birth")
-        self.birth.place(x = 300, y = 50)
+        self.birth.place(x = 350, y = 50)
 
         self.birth_entry = tk.Entry(self)
-        self.birth_entry.place(x = 375, y = 50)
+        self.birth_entry.place(x = 425, y = 50)
 
         # Ask the address
         self.address = tk.Label(self, text = "Address")
-        self.address.place(x = 40, y = 100)
+        self.address.place(x = 55, y = 100)
 
         self.address_entry = tk.Entry(self)
-        self.address_entry.place(x = 90, y = 100)
+        self.address_entry.place(x = 105, y = 100)
 
         # Ask the phone number
         self.number = tk.Label(self, text = "Cellphone Number")
-        self.number.place(x = 265, y = 100)
+        self.number.place(x = 315, y = 100)
 
         self.number_entry = tk.Entry(self)
-        self.number_entry.place(x = 375, y = 100)
+        self.number_entry.place(x = 425, y = 100)
 
         # Ask the email adress
+        self.email = tk.Label(self, text = "Email Address")
+        self.email.place(x = 23, y = 150)
+
+        self.email_entry = tk.Entry(self)
+        self.email_entry.place(x = 105, y = 150)
+
         # Ask the date of visit
         # Ask the vaccination status
         # Ask if there are symptoms of COVID-19
