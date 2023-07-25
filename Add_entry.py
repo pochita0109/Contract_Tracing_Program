8d21f3950af97934d389bbcb9e4160a40a88e354
pochita0109 <akosicosta@gmail.com>
2023-07-24 22:38:35 -0700

    Added a label and choices for Covid-19 test and fixed minor problems

--- Add_entry.py
+++ Add_entry.py
@@ -48,55 +48,73 @@ class add_entry(tk.Frame):
         
         # Ask the vaccination status
         self.vaccination = tk.Label(self, text = "Have you been vaccinated for COVID-19?")
-        self.vaccination.place(x = 30, y = 250)
+        self.vaccination.place(x = 30, y = 200)
 
         self.choice = tk.StringVar()
 
         self.choice1= tk.Radiobutton(self, text = "Not yet", variable = self.choice, value = "Not yet")
-        self.choice1.place(x = 30, y = 275)
+        self.choice1.place(x = 30, y = 225)
 
         self.choice2= tk.Radiobutton(self, text = "1st Dose", variable = self.choice, value = "1st Dose")
-        self.choice2.place(x = 30, y = 300)
+        self.choice2.place(x = 30, y = 250)
 
         self.choice3= tk.Radiobutton(self, text = "2nd Dose", variable = self.choice, value = "2nd Dose")
-        self.choice3.place(x = 30, y = 325)
+        self.choice3.place(x = 30, y = 275)
 
         self.choice4= tk.Radiobutton(self, text = "1st Booster Shot", variable = self.choice, value = "1st Booster Shot")
-        self.choice4.place(x = 30, y = 350)
+        self.choice4.place(x = 30, y = 300)
 
         self.choice5= tk.Radiobutton(self, text = "2nd Booster Shot", variable = self.choice, value = "2nd Booster Shot")
-        self.choice5.place(x = 30, y = 375)
+        self.choice5.place(x = 30, y = 325)
 
         # Ask if there are symptoms of COVID-19
         self.symptom = tk.Label(self, text = "Do you have a symptoms of COVID-19?")
-        self.symptom.place(x = 30, y = 425)
+        self.symptom.place(x = 30, y = 375)
 
         self.symptom_choice = tk.StringVar()
 
         self.symptom_choice1 = tk.Radiobutton(self, text = "Yes", variable = self.symptom_choice, value = "Yes")
-        self.symptom_choice1.place(x = 30, y = 450)
-
+        self.symptom_choice1.place(x = 30, y = 400)
         
         self.symptom_choice2 = tk.Radiobutton(self, text = "No", variable = self.symptom_choice, value = "No")
-        self.symptom_choice2.place(x = 30, y = 475)
+        self.symptom_choice2.place(x = 30, y = 425)
         
         self.symptom_choice3 = tk.Radiobutton(self, text = "Uncertain", variable = self.symptom_choice, value = "Uncertain")
-        self.symptom_choice3.place(x = 30, y = 500)
+        self.symptom_choice3.place(x = 30, y = 450)
 
         # Ask if there are exposure on someone with COVID-19
         self.exposure = tk.Label(self, text = "Do you have an exposure on someone with COVID 19?")
-        self.exposure.place(x = 300, y = 425)
+        self.exposure.place(x = 300, y = 375)
 
         self.exposure_choice = tk.StringVar()
 
         self.exposure_choice1 = tk.Radiobutton(self, text = "Yes", variable = self.exposure_choice, value = "Yes")
-        self.exposure_choice1.place(x = 300, y = 450)
-
+        self.exposure_choice1.place(x = 300, y = 400)
         
         self.exposure_choice2 = tk.Radiobutton(self, text = "No", variable = self.exposure_choice, value = "No")
-        self.exposure_choice2.place(x = 300, y = 475)
+        self.exposure_choice2.place(x = 300, y = 425)
         
         self.exposure_choice3 = tk.Radiobutton(self, text = "Uncertain", variable = self.exposure_choice, value = "Uncertain")
-        self.exposure_choice3.place(x = 300, y = 500)
+        self.exposure_choice3.place(x = 300, y = 450)
         
         # Ask if tested positive on COVID-19 in the last 14 days
+        self.test = tk.Label(self, text = "Have you been tested for COVID-19 in the last 14 days?")
+        self.test.place(x = 300, y = 200)
+
+        self.test_choice = tk.StringVar()
+
+        self.test_choice1 = tk.Radiobutton(self, text = "No", variable = self.test_choice, value = "No")
+        self.test_choice1.place(x = 300, y = 225)
+
+        
+        self.test_choice2 = tk.Radiobutton(self, text = "Yes - Positive", variable = self.test_choice, value = "Yes - Positive")
+        self.test_choice2.place(x = 300, y = 250)
+        
+        self.test_choice3 = tk.Radiobutton(self, text = "Yes - Negative", variable = self.test_choice, value = "Yes - Negative")
+        self.test_choice3.place(x = 300, y = 275)
+
+        self.test_choice4 = tk.Radiobutton(self, text = "Yes - Pending", variable = self.test_choice, value = "Yes - Pending")
+        self.test_choice4.place(x = 300, y = 300)
+
+        # Added label for personal information and for COVID-19 information
+        self.personal = tk.Label(self, text = "PERSONAL INFORMATION", font=("Helvetica", 20)).pack()
