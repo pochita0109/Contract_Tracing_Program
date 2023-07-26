5f73c72d7c1545e7a45d508c06c9ce92bb953be1
pochita0109 <akosicosta@gmail.com>
2023-07-26 12:43:56 -0700

    Added label, entry, and button for search entry

--- Main_page.py
+++ Main_page.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from Add_entry import add_entry
+from Search_entry import search_entry
 
 class main_page(tk.Tk):
     def __init__(self):
@@ -10,12 +11,16 @@ class main_page(tk.Tk):
         self.add = tk.Button(self, text = "   Add Entry   ", command = main_page.go_to_add_entry)
         self.add.place(x = 265, y = 225)
 
-        self.search = tk.Button(self, text = "   Search Entry   ")
+        self.search = tk.Button(self, text = "   Search Entry   ", command = main_page.go_to_search_entry)
         self.search.place(x = 260, y = 300)
         
     def go_to_add_entry():
         entry = add_entry()
         entry.place(relwidth = 1, relheight = 1)
+
+    def go_to_search_entry():
+        search = search_entry()
+        search.place(relwidth = 1, relheight = 1)
 
 if __name__=="__main__":
     app = main_page()
--- Search_entry.py
+++ Search_entry.py
@@ -4,3 +4,14 @@ import csv
 class search_entry(tk.Frame):
     def __init__(self, master = None):
         super().__init__(master)
+        self.search_title = tk.Label(self, text = "SEARCH YOUR ENTRY HERE", font = ("Helvetica", 20))
+        self.search_title.place(x = 110, y = 25)
+
+        self.search_label = tk.Label(self, text = "Enter Name to Search:")
+        self.search_label.place(x = 125, y = 100)
+
+        self.search_entry = tk.Entry(self)
+        self.search_entry.place(x = 250, y = 100)
+
+        self.search_button = tk.Button(self, text= "  Search  ")
+        self.search_button.place(x = 385, y = 100)
